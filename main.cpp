07a83c661aa09fd054b537f682735a7af71341b7
lionel@lionel-pc2 <lionel.desorme@free.fr>
2023-01-21 19:02:28 +0100

    Test epoch

--- main.cpp
+++ main.cpp
@@ -4,4 +4,15 @@
 int main(int argc, char** argv)
 {
 	std::cout << "Hello world" << std::endl;
+	
+	auto now = std::chrono::system_clock::now();
+
+#ifdef _WIN32
+	// operator<< not supported on gcc
+	std::cout << now << std::endl;
+#endif
+
+	auto duration = now.time_since_epoch();
+	auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+	std::cout << "millis: " << millis << std::endl;
 }
